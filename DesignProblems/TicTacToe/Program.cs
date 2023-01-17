c408bc1ebf8d076f41803255b7fa4cc727301e23
Monisha N V <monishanv@Monishas-MacBook-Air.local>
2023-01-17 11:05:53 +0530

    tic tac toe basic version

--- DesignProblems/TicTacToe/Program.cs
+++ DesignProblems/TicTacToe/Program.cs
@@ -10,17 +10,22 @@ string player2Name = Console.ReadLine();
 Players player1 = new Players(player1Name, 1);
 Players player2 = new Players(player2Name, -1);
 Players currentPlayer;
+
 Board ourBoard = new Board(player1);
 Players winner = null;
 ourBoard.GetBoardStatus();
+
 int count = 0;
 while(count < 9)
 {
     if (count % 2 == 0) { currentPlayer = player1; }
     else currentPlayer = player2;
+
     Console.WriteLine($"{currentPlayer.Name}'s turn");
+
     Console.WriteLine("enter row:");
     string row = Console.ReadLine();
+
     Console.WriteLine("enter col:");
     string col = Console.ReadLine();
 
